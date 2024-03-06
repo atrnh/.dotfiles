bd5d08abc4776fb8627a63d9a412081ad3fddca8
Ashley Trinh <itsashley@hey.com>
2024-03-06 15:24:36 -0800

    update

--- zsh/plugins/eza/eza.zsh
+++ zsh/plugins/eza/eza.zsh
@@ -3,13 +3,13 @@ if [ "$(command -v eza)" ]; then
   unalias l
   unalias la
   unalias ls
-  alias ls="eza -G --color auto --icons -a -s type"
-  alias la="eza -G --color auto --icons -a -s type"
-  alias l="eza -G --color auto --icons -a -s type"
+  alias ls="eza -G --color always --icons -a -s type"
+  alias la="eza -G --color always --icons -a -s type"
+  alias l="eza -G --color always --icons -a -s type"
   alias ll="eza -l --color always --icons -a -s type"
 
   tree() {
-    eza --tree --color auto --icons $@
+    eza --tree --color always --icons $@
   }
 fi
 

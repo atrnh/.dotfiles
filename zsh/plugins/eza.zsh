ef2c6e2f1cfd4ac7e711a7c00571f1ff2e546552
Ashley Trinh <itsashley@hey.com>
2024-07-24 19:00:15 -0700

    update

--- zsh/plugins/eza.zsh
+++ zsh/plugins/eza.zsh
@@ -1,21 +1,9 @@
 if [ "$(command -v eza)" ]; then
-  unalias ll
-  unalias l
-  unalias la
-  unalias ls
   alias ls="eza -G --color always --icons -a -s type"
-  alias la="eza -G --color always --icons -a -s type"
   alias l="eza -G --color always --icons -a -s type"
-  alias ll="eza -l --color always --icons -a -s type"
+  alias ll="eza --git -l --no-user --color always --icons -a -s type"
 
   tree() {
     eza --tree --color always --icons $@
   }
 fi
-
-# git status
-gs() {
-  if [ "$(command -v eza)" ]; then eza --git --long --no-permissions --no-filesize --no-time --no-user --icons --color always
-  else git status $@
-  fi
-}
